3e8b5787f2703fa93420a9a5a482de567f79b827
zdia <z-dia@t-online.de>
2013-09-25 08:50:54 +0200

    textile takes args from commandline

--- textile.php
+++ textile.php
@@ -7,13 +7,12 @@ require_once('classTextile.php');
 
 $textile = new Textile;
 
-$file_parts = pathinfo($argv[1]);
+// $file_parts = pathinfo($argv[1]);
+// $wiki = file_get_contents($file_parts['basename']);
+// $html = $textile->TextileThis($wiki);
+echo $textile->TextileThis($argv[1]);
 
-$wiki = file_get_contents($file_parts['basename']);
-
-$html = $textile->TextileThis($wiki);
-
-file_put_contents($file_parts['filename'].".html", $html);
+// file_put_contents($file_parts['filename'].".html", $html);
 
 // For untrusted user input, use TextileRestricted instead:
 // echo $textile->TextileRestricted($in);
